7f3987391a3c23df2682f8f660c70e8e5a21728b
ConnorCallahan <Connor@MacBook-Pro.local>
2018-01-29 12:46:47 -0800

    update db user

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://<ConnorCallahan>:<1LovemLab!!!>@ds117858.mlab.com:17858/woodnext-prod' }
+    module.exports = {mongoURI: 'mongodb://<sinatra>:<test!!!testing!!!>@ds117858.mlab.com:17858/woodnext-prod' }
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/woodsnext-dev'}
 }
