4a5a837c0b82f2db227b450055314d0208038b43
ConnorCallahan <Connor@MacBook-Pro.local>
2018-01-30 10:43:22 -0800

    change password feature

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
+const {ensureAuthenticated} = require('../helpers/auth');
 
 var router = express.Router();
 
@@ -15,6 +16,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// Login
 router.get('/login', (req, res) => {
   res.render('users/login', {
     title: 'Login',
@@ -29,10 +31,18 @@ router.get('/logout', (req, res) => {
   res.redirect('/users/login');
 });
 
+// Register
 router.get('/register', (req, res) => {
   res.render('users/register', {
     title: 'Register',
     bgImage: '../images/tools.png'
+  });
+});
+
+// Settings
+router.get('/settings', ensureAuthenticated, (req, res) => {
+  res.render('users/settings', {
+    title: 'Settings'
   });
 });
 
@@ -93,6 +103,51 @@ router.post('/register', (req, res) => {
         }
       })
   }
+});
+
+// Settings post form
+router.put('/settings', ensureAuthenticated, (req, res) => {
+  let errors = [];
+  if(req.body.currentPassword == req.body.newPassword){
+    errors.push({text: 'Passwords cannot match'});
+  }
+  if(req.body.newPassword.length < 4){
+    errors.push({text: 'New password must be more than 4 characters'});
+  }
+  if(errors.length > 0) {
+    res.render('users/settings',{
+      errors: errors
+    });
+  } else {
+    User.findOne({email: req.user.email})    
+      .then(user => {
+        //Match password
+        bcrypt.compare(req.body.currentPassword, user.password, (err, isMatch) => {
+          if(err) throw err;
+          if(isMatch){
+            bcrypt.genSalt(10, (err, salt) => {
+              bcrypt.hash(req.body.newPassword,salt,(err, hash) => {
+                if(err) throw err;
+                user.password = hash;
+                user.save()
+                  .then(user => {
+                    req.flash('success_msg','Password updated');
+                    res.redirect('/ideas');
+                  })
+                  .catch(err => {
+                    console.log(err);
+                    return;
+                  });
+              });
+            });
+          } else {
+            req.flash('error_msg', 'Current password does not match');
+            res.redirect('/users/settings');
+            return;
+          }
+        });
+      });
+  };
 });
 
 module.exports = router;
