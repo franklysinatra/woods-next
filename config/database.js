a13086fde0af7913bbe2a6a4851397532bbee9ca
ConnorCallahan <Connor@MacBook-Pro.local>
2018-01-29 20:53:30 -0800

    add new button when loggedin

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports = {mongoURI: 'mongodb://sinatra:test!!!testing!!!@ds117858.mlab.com:17858/woodnext-prod' }
 } else {
-    module.exports = {mongoURI: 'mongodb://localhost/woodsnext-dev'}
+    module.exports = {mongoURI: 'mongodb://127.0.0.1/woodsnext-dev'}
 }
