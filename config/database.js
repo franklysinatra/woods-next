66ebbadc560d31ec9328311fbe196ff6df5424e7
ConnorCallahan <Connor@MacBook-Pro.local>
2018-02-01 09:57:37 -0800

    update gitignore

--- config/database.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://sinatra:test!!!testing!!!@ds117858.mlab.com:17858/woodnext-prod' }
-} else {
-    module.exports = {mongoURI: 'mongodb://127.0.0.1/woodsnext-dev'}
-}
